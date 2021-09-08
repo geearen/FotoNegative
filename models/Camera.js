09ce6580d5dfb30770793625408c7a481a5b3d85
geearen <allengonzales018@gmail.com>
2021-09-07 23:12:55 -0700

    add Camera model

---
+++ models/Camera.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+
+const cameraSchema = new mongoose.Schema(
+  {
+    cameraName: {
+      type: String,
+      required: [true, "name can not be empty"],
+    },
+    brandName:{
+      type: String,
+    },
+    cameraImage: {
+      type: String,
+      required: [true, "image can not be empty"],
+    },
+    yearReleased: {
+      type: Number,
+    },
+    interchangeableLens:{
+      type: Boolean,
+    },
+    imageFormat:{
+      type: String,
+    },
+    categories:{
+      type: [String]
+    },
+    photographyType:{
+      type:String,
+    },
+    imageFormat:{
+      type: String
+    }
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const Camera = mongoose.model("Camera", cameraSchema);
+
+module.exports = Camera;
