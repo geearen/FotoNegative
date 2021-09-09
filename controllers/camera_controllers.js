dd135136dd3fc2192f353e5f719616e5261c01c5
geearen <allengonzales018@gmail.com>
2021-09-09 15:32:17 -0700

    refactored update route for camera

--- controllers/camera_controllers.js
+++ controllers/camera_controllers.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const{Camera} = require("../models");
+const { Camera } = require("../models");
 
 const apiKey = process.env.UNSPLASH_APP_API_KEY;
 
 /* Index Route */
-router.get("/", async(req, res, next) =>{
-  try{
+router.get("/", async (req, res, next) => {
+  try {
     const allCameras = await Camera.find({});
     const context = {
       cameras: allCameras,
     };
     return res.render("cameras/index", context);
-  }catch(error){
+  } catch (error) {
     console.log(error);
     req.error = error;
     return next();
@@ -20,35 +20,35 @@ router.get("/", async(req, res, next) =>{
 });
 
 /* New Route */
-router.get("/new", function(req, res){
+router.get("/new", function (req, res) {
   const context = {};
   return res.render("cameras/new", context);
 });
 
 /* Create Route */
-router.post("/", async (req, res) =>{
-  try{
+router.post("/", async (req, res) => {
+  try {
     const createdCamera = await Camera.create(req.body);
-    return res.redirect('/cameras');
-  }catch(error){
-    const context = {error};
+    return res.redirect("/cameras");
+  } catch (error) {
+    const context = { error };
     console.log(error);
-    return res.render("cameras/new")
+    return res.render("cameras/new");
   }
-})
+});
 
 //api "https://api.unsplash.com/photos/random?query=film-&orientation=portrait&content_safety=high&client_id=dnxslX9NqG7sdJIFpYSn_YeO8crKLlvk1r65XylTr1o"
 
 /* Show Page */
 router.get("/:id", async (req, res, next) => {
-  try{
+  try {
     const foundCamera = await Camera.findById(req.params.id);
 
-    const context ={
+    const context = {
       camera: foundCamera,
-    }
+    };
     return res.render("cameras/show", context);
-  }catch(error){
+  } catch (error) {
     console.log(error);
     req.error = error;
     return next();
@@ -56,27 +56,38 @@ router.get("/:id", async (req, res, next) => {
 });
 
 /* Edit Page */
-router.get("/:id/edit", async (req, res) =>{
-  try{
-    const foundCamera = await Camera.findById(req.params.id)
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const foundCamera = await Camera.findById(req.params.id);
     const context = {
-      camera: foundCamera
-    }
+      camera: foundCamera,
+    };
     return res.render("cameras/edit", context);
-  }catch(error){
+  } catch (error) {
     console.log(error);
     req.error = error;
     return next();
   }
-})
+});
 
 /* Update Page */
-router.put("/:id", function(req, res){
-  res.send("Updated Camera")
-})
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedCamera = await Camera.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+      );
+    return res.redirect(`/cameras/${updatedCamera.id}`)
+  } catch(error){
+    console.log(error);
+    req.error = error;
+    return next();
+  }
+});
 
 /* Delete Page */
-router.delete("/:id", function(req, res){
+router.delete("/:id", function (req, res) {
   res.send("Deleted Camera");
 });
 
