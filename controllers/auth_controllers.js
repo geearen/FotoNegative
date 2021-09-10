c5925efaaba124569185cdd39c4126a4b279941f
geearen <allengonzales018@gmail.com>
2021-09-10 11:01:10 -0700

    refactored post login route

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -5,14 +5,15 @@ const {User} = require("../models");
 
 /* Register route  */
 router.get("/register", function (req, res){
-  const error = null;
-  const context = {error}
+  const context = {error: null}
   return res.render("auth/register", context);
 });
 
 /* Login route */
 router.get("/login", function(req, res){
-  return res.render("auth/login")
+  // const error = null;
+  const context = { error: null };
+  return res.render("auth/login", context)
 });
 
 /* Register Post route */
@@ -21,13 +22,15 @@ router.post("/register", async (req, res) =>{
     const foundUser = await User.exists({$or:[{email: req.body.email}, {username: req.body.username}]})
     
     if(foundUser){
-      const context = {error: "Username or Email Address already exist.",}
-      return res.render("auth/register", context)
+      // const context = {error: "Username or Email Address already exist.",}
+      // return res.render("auth/register", context)
+      throw "Username or Email Address already exist.";
     };
 
     if(req.body.password !== req.body.password2) {
-      const context = { error: "Password Does Not Match." };
-      return res.render("auth/register", context);
+      // const context = { error: "Password Does Not Match." };
+      // return res.render("auth/register", context);
+      throw "Password Does Not Match."
     }
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(req.body.password, salt);
@@ -38,13 +41,34 @@ router.post("/register", async (req, res) =>{
     return res.redirect("/login");
   }catch(error){
     console.log(error);
-    return res.send(error);
+    // return res.send(error);
+    console.log(error);
+    const context ={error}
+    return res.render("auth/register", context)
   }
 })
 
 /* Login Post Route */
-router.post("/login", function(req, res){
-  res.send("Account Login");
+router.post("/login", async (req, res) =>{
+  try {
+    const foundUser = await User.findOne({username:req.body.username});
+
+    if(!foundUser)throw "Username and/or Password does not match";
+
+    const match = await bcrypt.compare(req.body.password, foundUser.password);
+    if(!match) throw "Username and/or Password does not match";
+
+    req.session.currentUser ={
+      id:foundUser._id,
+      username:foundUser.username,
+    }
+
+    return res.redirect("/home");
+  } catch (error) {
+    console.log(error);
+    const context = {error}
+    return res.render("auth/login", context)
+  }
 })
 
 /* Log Out Route */
